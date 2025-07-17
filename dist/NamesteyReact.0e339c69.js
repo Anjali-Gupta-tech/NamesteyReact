5b2ea20d3d1c179648bb1c2fb0099d89cc8cca30
Anjali-Gupta-tech <aanjaligupta1503@gamil.com>
2025-07-17 17:53:53 +0530

    Removed .parcel-cache and updated .gitignore

--- dist/NamesteyReact.0e339c69.js
+++
@@ -1,2 +0,0 @@
-let parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"This is a head tag"),React.createElement("h1",{},"This is a head tag")]));console.log(parent);let root=ReactDOM.createRoot(document.getElementById("root"));root.render(parent);
-//# sourceMappingURL=NamesteyReact.0e339c69.js.map
